bb94cc9ad43b5dc2f0982662832a62d04c2bc60a
David Nadlinger <code@klickverbot.at>
2018-08-25 00:50:03 +0100

    setup: Install icon files

--- setup.py
+++ setup.py
@@ -2,9 +2,10 @@ from setuptools import setup
 
 setup(
     name='ndscan',
-    version='0.0.1',
+    version='0.0.2',
     url='https://github.com/klickverbot/ndscan',
     author='David P. Nadlinger',
     packages=['ndscan'],
+    package_data={'ndscan': ['icons/*.png']},
     entry_points={},
 )
