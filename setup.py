bea7a1f19847e31ab90e83df79f886a5e7b1b988
David Nadlinger <code@klickverbot.at>
2018-08-25 02:03:01 +0100

    setup: Also copy svg icons

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup
 
 setup(
     name='ndscan',
-    version='0.0.2',
+    version='0.1.0',
     url='https://github.com/klickverbot/ndscan',
     author='David P. Nadlinger',
     packages=['ndscan'],
-    package_data={'ndscan': ['icons/*.png']},
+    package_data={'ndscan': ['icons/*.png', 'icons/*.svg']},
     entry_points={},
 )
