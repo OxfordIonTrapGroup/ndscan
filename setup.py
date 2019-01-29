4b2b1ebb846117f27fdb2d3dac5fd9e27e7ee307
David Nadlinger <code@klickverbot.at>
2019-01-29 19:23:27 +0000

    setup: Add ARTIQ dependence and some metadata
    Licence choice is just copied from ARTIQ from now; it is not
    very applicable to scientific users anyway.

--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='ndscan',
-    version='0.1.0',
-    url='https://github.com/klickverbot/ndscan',
-    author='David P. Nadlinger',
+    name="ndscan",
+    version="0.1.0",
+    url="https://github.com/klickverbot/ndscan",
+    description="Composable experiment fragments and multidimensional scans for ARTIQ",
+    license="LGPLv3+",
+    author="David Nadlinger",
     packages=find_packages(),
-    package_data={'ndscan': ['icons/*.png', 'icons/*.svg']},
+    package_data={"ndscan": ["icons/*.png", "icons/*.svg"]},
+    install_requires={
+        "artiq"
+    }
 )
