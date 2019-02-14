69fdbec4ac942276e713292dd4a995fe0758c686
David Nadlinger <code@klickverbot.at>
2019-02-14 21:20:11 +0000

    docs: Switch to rtd Sphinx theme
    None of the themes I tried handle type annotations well, but
    Alabaster displays them particularly poorly.
    
    Also configures "Edit on GitHub" links.

--- docs/conf.py
+++ docs/conf.py
@@ -75,14 +75,24 @@ pygments_style = None
 
 # -- Options for HTML output -------------------------------------------------
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
-html_theme_options = {}
+html_theme_options = {
+    'canonical_url': 'https://oxfordiontrapgroup.github.io/ndscan/',
+}
+
+html_context = {
+    'display_github': True,
+    'github_user': 'OxfordIonTrapGroup',
+    'github_repo': 'ndscan',
+    'github_version': 'master',
+    'conf_py_path': '/docs/'
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+# html_static_path = ['_static']
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
