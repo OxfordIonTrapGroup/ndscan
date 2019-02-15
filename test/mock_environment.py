754258592e89103d2240e445f6848bf6d6454164
David Nadlinger <code@klickverbot.at>
2019-02-15 00:58:49 +0000

    test: Add environment mocks and simple subscan test

---
+++ test/mock_environment.py
@@ -0,0 +1,65 @@
+"""
+Helpers for unit-testing HasEnvironment code.
+"""
+
+import copy
+import unittest
+
+from artiq.master.worker_db import DatasetManager, DeviceManager
+from artiq.protocols.sync_struct import process_mod
+
+
+class MockDatasetDB:
+    def __init__(self):
+        self.data = dict()
+
+    def get(self, key):
+        return self.data[key][1]
+
+    def update(self, mod):
+        # Copy mod before applying to avoid sharing references to objects
+        # between this and the DatasetManager, which would lead to mods being
+        # applied twice.
+        process_mod(self.data, copy.deepcopy(mod))
+
+    def delete(self, key):
+        del self.data[key]
+
+
+class MockScheduler:
+    def check_pause(self):
+        return False
+
+    def pause(self):
+        pass
+
+
+class MockDeviceDB:
+    def __init__(self):
+        self.devices = {"core": {"type": "dummy"}}
+
+    def get(self, key):
+        return self.devices[key]
+
+    def get_device_db(self):
+        return self.devices
+
+
+class HasEnvironmentCase(unittest.TestCase):
+    def setUp(self):
+        self.dataset_db = MockDatasetDB()
+        self.dataset_mgr = DatasetManager(self.dataset_db)
+        self.device_db = MockDeviceDB()
+        self.scheduler = MockScheduler()
+        self.device_mgr = DeviceManager(
+            self.device_db, virtual_devices={"scheduler": self.scheduler})
+
+    def create(self, klass, *args, **kwargs):
+        return klass((self.device_mgr, self.dataset_mgr, None), *args, **kwargs)
+
+
+class ExpFragmentCase(HasEnvironmentCase):
+    def create(self, klass, *args, **kwargs):
+        fragment = super().create(klass, [], *args, **kwargs)
+        fragment.init_params()
+        return fragment
