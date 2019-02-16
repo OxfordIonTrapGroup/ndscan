ddc2c218df167e566f2e7b3e28eb0ec7f6c27aee
David Nadlinger <code@klickverbot.at>
2019-02-16 01:16:02 +0000

    test: {Echo -> AddOne}Fragment

--- test/test_experiment.py
+++ test/test_experiment.py
@@ -4,10 +4,10 @@ Tests for ndscan.experiment top-level runners.
 
 import json
 from ndscan.experiment import make_fragment_scan_exp
-from fixtures import EchoFragment
+from fixtures import AddOneFragment
 from mock_environment import HasEnvironmentCase
 
-ScanEchoExp = make_fragment_scan_exp(EchoFragment)
+ScanEchoExp = make_fragment_scan_exp(AddOneFragment)
 
 
 class FragmentScanExpCase(HasEnvironmentCase):
@@ -21,12 +21,12 @@ class FragmentScanExpCase(HasEnvironmentCase):
             return self.dataset_db.get("ndscan." + key)
 
         self.assertEqual(json.loads(d("axes")), [])
-        self.assertEqual(d("fragment_fqn"), "fixtures.EchoFragment")
+        self.assertEqual(d("fragment_fqn"), "fixtures.AddOneFragment")
         self.assertEqual(d("rid"), 0)
 
     def test_run_1d_scan(self):
         exp = self.create(ScanEchoExp)
-        fqn = "fixtures.EchoFragment.value"
+        fqn = "fixtures.AddOneFragment.value"
         exp._params["scan"]["axes"].append({
             "type": "linear",
             "range": {
@@ -63,5 +63,5 @@ class FragmentScanExpCase(HasEnvironmentCase):
             }])
         self.assertEqual(d("points.axis_0"), [0, 1, 2])
         self.assertEqual(d("points.channel_result"), [1, 2, 3])
-        self.assertEqual(d("fragment_fqn"), "fixtures.EchoFragment")
+        self.assertEqual(d("fragment_fqn"), "fixtures.AddOneFragment")
         self.assertEqual(d("rid"), 0)
