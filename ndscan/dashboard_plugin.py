605dcf8d3c067f87c038a91a5a5de1c516e095ff
David Nadlinger <code@klickverbot.at>
2019-05-03 15:36:23 +0100

    Add doc comments to a few modules/classes [nfc]

--- ndscan/dashboard_plugin.py
+++ ndscan/dashboard_plugin.py
@@ -1,3 +1,10 @@
+"""ARTIQ dashboard plugin; loads an argument editor to  edit ndscan parameters.
+
+Pass to ``artiq_dashboard`` using the ``--load-plugin/-p`` switch to load, e.g.::
+
+    artiq_dashboard -s 10.255.6.191 -p ndscan.dashboard_plugin
+"""
+
 from artiq.dashboard.experiments import ExperimentManager
 from .dashboard.argument_editor import ArgumentEditor
 
