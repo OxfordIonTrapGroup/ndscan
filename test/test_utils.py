49f7a83adad9d215128377c05d25c54437ce4338
David Nadlinger <code@klickverbot.at>
2019-09-21 02:26:04 +0100

    test: Add shorten_to_unambiguous_suffixes unit test

--- test/test_utils.py
+++ test/test_utils.py
@@ -1,5 +1,5 @@
 import unittest
-from ndscan.utils import strip_prefix, strip_suffix
+from ndscan.utils import strip_prefix, strip_suffix, shorten_to_unambiguous_suffixes
 
 
 class StripTest(unittest.TestCase):
@@ -10,3 +10,15 @@ class StripTest(unittest.TestCase):
     def test_strip_suffix(self):
         self.assertEqual(strip_suffix("foo_bar", "foo_"), "foo_bar")
         self.assertEqual(strip_suffix("foo_bar", "_bar"), "foo")
+
+
+class ShortenTest(unittest.TestCase):
+    def test_shorten(self):
+        def test(expected):
+            self.assertEqual(
+                shorten_to_unambiguous_suffixes(
+                    expected.keys(), lambda fqn, n: "/".join(fqn.split("/")[-n:])),
+                expected)
+
+        test({"foo": "foo"})
+        test({"": "", "foo/bar": "foo/bar", "foo/baz": "baz", "baz/bar": "baz/bar"})
