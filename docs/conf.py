b044cb360fcb823746965cc71af2566705987881
David Nadlinger <code@klickverbot.at>
2020-07-11 14:49:57 +0100

    docs: Fix table line wrapping in RTD theme
    Without this, there is no line wrapping in tables at all,
    leading to humongous horizontal scroll bars on the
    display_hints table.

--- docs/conf.py
+++ docs/conf.py
@@ -93,13 +93,16 @@ html_context = {
     'github_user': 'OxfordIonTrapGroup',
     'github_repo': 'ndscan',
     'github_version': 'master',
-    'conf_py_path': '/docs/'
+    'conf_py_path': '/docs/',
+    'css_files': [
+        '_static/theme_overrides.css',  # override wide tables in RTD theme
+    ],
 }
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-# html_static_path = ['_static']
+html_static_path = ['_static']
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
