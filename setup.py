1ec35c23c671d346e354d8bb9f7497a0d94a3c8d
David Nadlinger <code@klickverbot.at>
2021-03-23 23:33:36 +0000

    Increase version number to 0.2.0
    This doesn't mark any special level of maturity, and we have not
    adopted any sort of formal release model yet. The main impetus for
    this was to sort out some internal CI version resolution issues.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="ndscan",
-    version="0.1.0",
+    version="0.2.0",
     url="https://github.com/klickverbot/ndscan",
     description="Composable experiment fragments and multidimensional scans for ARTIQ",
     license="LGPLv3+",
