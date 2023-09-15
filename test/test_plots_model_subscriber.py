92dfacdbfef4e3c9619317852ffe53e20d10e9d2
David Nadlinger <code@klickverbot.at>
2023-09-15 16:10:42 +0100

    ARTIQ 8 (Git master) compatibility
    The SimpleApplet API was changed in m-labs/artiq@fe0f6d8a2c0
    (with the new dataset metadata being introduced) in a
    backwards-incompatible fashion.
    
    There is now also a built-in way to call set_dataset() without
    a separate RPC connection. This also turned out to be
    backwards-incompatible, as the --port options would conflict.
    
    Currently, this is not backwards-compatible to ARTIQ 7.

--- test/test_plots_model_subscriber.py
+++ test/test_plots_model_subscriber.py
@@ -13,7 +13,7 @@ class SinglePointTest(unittest.TestCase):
         self.context = Context()
         self.root = SubscriberRoot("ndscan.", self.context)
         self.datasets = Notifier({
-            "ndscan.axes": (False, "[]"),
+            "ndscan.axes": (False, "[]", {}),
             "ndscan.channels": (False,
                                 json.dumps({
                                     "foo": {
@@ -28,8 +28,8 @@ class SinglePointTest(unittest.TestCase):
                                         "type": "int",
                                         "unit": ""
                                     }
-                                })),
-            ("ndscan." + SCHEMA_REVISION_KEY): (False, SCHEMA_REVISION),
+                                }), {}),
+            ("ndscan." + SCHEMA_REVISION_KEY): (False, SCHEMA_REVISION, {}),
         })
         self.pending_mods = []
         self.datasets.publish = lambda a: self.pending_mods.append(a)
@@ -42,69 +42,70 @@ class SinglePointTest(unittest.TestCase):
         self.sync()
 
     def sync(self):
-        self.root.data_changed(self.datasets.raw_view, self.pending_mods)
+        values = {k: v[1] for k, v in self.datasets.raw_view.items()}
+        self.root.data_changed(values, self.pending_mods)
         self.pending_mods.clear()
 
     def test_new_point(self):
         self.init()
-        self.datasets["ndscan.point.foo"] = (False, 42)
-        self.datasets["ndscan.point.bar"] = (False, 23)
-        self.datasets["ndscan.point_phase"] = (False, True)
+        self.datasets["ndscan.point.foo"] = (False, 42, {})
+        self.datasets["ndscan.point.bar"] = (False, 23, {})
+        self.datasets["ndscan.point_phase"] = (False, True, {})
         self.sync()
         self.assertEqual(self.root.get_model().get_point(), {"foo": 42, "bar": 23})
 
     def test_halfway(self):
-        self.datasets["ndscan.point.foo"] = (False, 42)
+        self.datasets["ndscan.point.foo"] = (False, 42, {})
         self.init()
 
         with self.assertRaises(ValueError):
             # No complete point yet.
             self.root.get_model().get_point()
 
-        self.datasets["ndscan.point.bar"] = (False, 23)
-        self.datasets["ndscan.point_phase"] = (False, True)
+        self.datasets["ndscan.point.bar"] = (False, 23, {})
+        self.datasets["ndscan.point_phase"] = (False, True, {})
         self.sync()
         self.assertEqual(self.root.get_model().get_point(), {"foo": 42, "bar": 23})
 
     def test_one_and_a_half(self):
-        self.datasets["ndscan.point.foo"] = (False, 42)
+        self.datasets["ndscan.point.foo"] = (False, 42, {})
         self.init()
 
         with self.assertRaises(ValueError):
             # No complete point yet.
             self.root.get_model().get_point()
 
-        self.datasets["ndscan.point.bar"] = (False, 23)
-        self.datasets["ndscan.point_phase"] = (False, True)
+        self.datasets["ndscan.point.bar"] = (False, 23, {})
+        self.datasets["ndscan.point_phase"] = (False, True, {})
 
         # Already write foo value of next point.
-        self.datasets["ndscan.point.foo"] = (False, 0)
+        self.datasets["ndscan.point.foo"] = (False, 0, {})
         self.sync()
 
         # Foo should still be the old value.
         self.assertEqual(self.root.get_model().get_point(), {"foo": 42, "bar": 23})
 
     def test_preexisting(self):
-        self.datasets["ndscan.point.foo"] = (False, 42)
-        self.datasets["ndscan.point.bar"] = (False, 42)
-        self.datasets["ndscan.point_phase"] = (False, True)
-        self.datasets["ndscan.point.foo"] = (False, 0)
+        self.datasets["ndscan.point.foo"] = (False, 42, {})
+        self.datasets["ndscan.point.bar"] = (False, 42, {})
+        self.datasets["ndscan.point_phase"] = (False, True, {})
+        self.datasets["ndscan.point.foo"] = (False, 0, {})
         self.init()
 
         with self.assertRaises(ValueError):
             # Can't know whether point is complete (it indeed isn't).
             self.root.get_model().get_point()
 
-        self.datasets["ndscan.point.bar"] = (False, 1)
-        self.datasets["ndscan.point_phase"] = (False, False)
+        self.datasets["ndscan.point.bar"] = (False, 1, {})
+        self.datasets["ndscan.point_phase"] = (False, False, {})
         self.sync()
 
         self.assertEqual(self.root.get_model().get_point(), {"foo": 0, "bar": 1})
 
     def test_already_completed(self):
-        self.datasets["ndscan.point.foo"] = (False, 42)
-        self.datasets["ndscan.point.bar"] = (False, 23)
-        self.datasets["ndscan.point_phase"] = (False, True)
-        self.datasets["ndscan.completed"] = (False, True)
+        self.datasets["ndscan.point.foo"] = (False, 42, {})
+        self.datasets["ndscan.point.bar"] = (False, 23, {})
+        self.datasets["ndscan.point_phase"] = (False, True, {})
+        self.datasets["ndscan.completed"] = (False, True, {})
         self.init()
         self.assertEqual(self.root.get_model().get_point(), {"foo": 42, "bar": 23})
