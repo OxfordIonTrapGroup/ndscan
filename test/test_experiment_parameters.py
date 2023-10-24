8708e21981b05dafc5d8bb12f6114192acc171ac
Peter Drmota <peter.drmota@physics.ox.ac.uk>
2023-10-24 15:57:55 +0100

    Revise EnumParam implementation on experiment side, with defaults in mind
    Needed a clear convention what qualifies as a default value. Either given as Enum object
    or as string that evaluates to the name of an Enum member. This allows the value of the
    Enum members to be arbitrary objects.
    For displaying purposes, the spec includes a dictionary mapping Enum member names to
    strings. If the values happen to be (descriptive) strings, those will be shown in the
    dashboard and applets. Otherwise, the Enum member names are mapped to themselves.

--- test/test_experiment_parameters.py
+++ test/test_experiment_parameters.py
@@ -1,7 +1,8 @@
 from mock_environment import HasEnvironmentCase
+from enum import Enum
 from ndscan.experiment.fragment import Fragment
-from ndscan.experiment.parameters import FloatParam, IntParam, BoolParam
-
+from ndscan.experiment.parameters import (FloatParam, IntParam, BoolParam,
+                                          enum_param_factory)
 
 # "Container class" to hide base class from unittest auto-discovery.
 class GenericBase:
@@ -104,3 +105,76 @@ class BoolParamCase(GenericBase.Cases):
             "is_scannable": True,
         },
     }
+
+
+class EnumParamCase(unittest.TestCase):
+    def test_describe(self):
+        class Options(Enum):
+            first = "A"
+            second = "B"
+            third = "C"
+
+        (EnumParam, _, _) = enum_param_factory(Options)
+
+        param = EnumParam("foo", "bar", Options.second)
+        self.assertEqual(
+            param.describe(), {
+                "fqn": "foo",
+                "description": "bar",
+                "type": f"enum_Options_{id(Options)}",
+                "default": "'second'",
+                "spec": {
+                    "enum_display_map": {o.name: o.value for o in Options},
+                    "is_scannable": True
+                }
+            })
+        param = EnumParam("foo", "bar", "'second'")
+        self.assertEqual(
+            param.describe(), {
+                "fqn": "foo",
+                "description": "bar",
+                "type": f"enum_Options_{id(Options)}",
+                "default": "'second'",
+                "spec": {
+                    "enum_display_map": {o.name: o.value for o in Options},
+                    "is_scannable": True
+                }
+            })
+
+    def test_evaluate_default(self):
+        class StrOptions(Enum):
+            first = "A"
+            second = "B"
+            third = "C"
+
+        class IntOptions(Enum):
+            first = 1
+            second = 2
+            third = 3
+
+        (StrEnumParam, _, _) = enum_param_factory(StrOptions)
+        (IntEnumParam, _, _) = enum_param_factory(IntOptions)
+
+        def mock_get_dataset(key: str, default=None):
+            try:
+                return {"baz": "third"}[key]
+            except KeyError:
+                return default
+
+        str_param = StrEnumParam("foo", "bar", StrOptions.second)
+        self.assertEqual(str_param.eval_default(mock_get_dataset), StrOptions.second)
+
+        str_param = StrEnumParam("foo", "bar", "dataset('baz', 'first')")
+        self.assertEqual(str_param.eval_default(mock_get_dataset), StrOptions.third)
+
+        str_param = StrEnumParam("foo", "bar", "dataset('bam', 'first')")
+        self.assertEqual(str_param.eval_default(mock_get_dataset), StrOptions.first)
+
+        int_param = IntEnumParam("foo", "bar", IntOptions.second)
+        self.assertEqual(int_param.eval_default(mock_get_dataset), IntOptions.second)
+
+        int_param = IntEnumParam("foo", "bar", "dataset('baz', 'first')")
+        self.assertEqual(int_param.eval_default(mock_get_dataset), IntOptions.third)
+
+        int_param = IntEnumParam("foo", "bar", "dataset('bam', 'first')")
+        self.assertEqual(int_param.eval_default(mock_get_dataset), IntOptions.first)
