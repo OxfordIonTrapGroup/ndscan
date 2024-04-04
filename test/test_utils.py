22e68e1e7ac21a287c94515dc24f9f084b208f5a
David Nadlinger <code@klickverbot.at>
2024-04-04 20:42:47 +0200

    utils.shorten_to_unambiguous_suffixes: Fix bug by simplified rewrite, document
    The added `["bar", "foo/bar"]` test case failed depending on the order
    in the old implementation.
    
    This new, much more straightforward implementation might lead to subtly
    different disambiguations in some cases, but if such cases in fact do
    exist, this seems like a worthwhile trade-off to make, since we never
    actually document the details of the shortening algorithm in the user
    visible parts, or state that it is unchanging. If the changes do turn
    out to be obnoxious for some established analysis pipeline, we could
    salvage the old implementation quite easily by skipping the "promotion"
    check if `old == current`, but rewriting this was quicker than
    convincing myself that this fix actually would address all the corner
    cases.

--- test/test_utils.py
+++ test/test_utils.py
@@ -1,4 +1,5 @@
 import unittest
+from itertools import permutations
 from ndscan.utils import strip_prefix, strip_suffix, shorten_to_unambiguous_suffixes
 
 
@@ -14,11 +15,16 @@ class StripTest(unittest.TestCase):
 
 class ShortenTest(unittest.TestCase):
     def test_shorten(self):
+        def shorten_at_slash(fqns):
+            return shorten_to_unambiguous_suffixes(
+                fqns, lambda fqn, n: "/".join(fqn.split("/")[-n:]))
+
         def test(expected):
-            self.assertEqual(
-                shorten_to_unambiguous_suffixes(
-                    expected.keys(), lambda fqn, n: "/".join(fqn.split("/")[-n:])),
-                expected)
+            # Test all orderings.
+            for keys in permutations(expected.keys()):
+                self.assertEqual(shorten_at_slash(keys), expected)
+
+        test({})
 
         test({"foo": "foo"})
         test({"": "", "foo/bar": "foo/bar", "foo/baz": "baz", "baz/bar": "baz/bar"})
@@ -27,3 +33,9 @@ class ShortenTest(unittest.TestCase):
         test({"a1/b/c": "a1/b/c", "a2/b/c": "a2/b/c"})
         test({"a1/b/c/d": "a1/b/c/d", "a2/b/c/d": "a2/b/c/d"})
         test({"a1/b/c/d/e": "a1/b/c/d/e", "a2/b/c/d/e": "a2/b/c/d/e"})
+
+        test({"bar": "bar", "foo/bar": "foo/bar"})
+
+        # Test repeated fqns.
+        with self.assertRaises(ValueError):
+            shorten_at_slash(["foo/bar", "foo/bar"])
