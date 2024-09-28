3c6527cd8035df1a5e2741374dd0dba0680327df
Charles Baynham <c.baynham@imperial.ac.uk>
2024-09-28 12:07:29 +0100

    Test failure of rebinding to an overridden parameter

--- test/test_experiment_fragment.py
+++ test/test_experiment_fragment.py
@@ -97,6 +97,22 @@ class TestRebinding(HasEnvironmentCase):
         self.assertEqual(result[trf.first.result], 3)
         self.assertEqual(result[trf.second.result], 3)
 
+    def test_transitive_rebind_with_override_fails(self):
+        class OverriddenTransitiveReboundAddOneFragment(ExpFragment):
+            def build_fragment(self):
+                self.setattr_fragment("first", AddOneFragment)
+                self.setattr_fragment("second", AddOneFragment)
+
+                self.first.override_param("value", 2)
+                self.second.bind_param("value", self.first.value)
+
+            def run_once(self):
+                self.first.run_once()
+                self.second.run_once()
+
+        with self.assertRaises(AssertionError):
+            self.create(OverriddenTransitiveReboundAddOneFragment, [])
+
     def test_invalid_bind(self):
         class InvalidBindFragment(ExpFragment):
             def build_fragment(self):
