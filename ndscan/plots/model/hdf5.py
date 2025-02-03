OxfordIonTrapGroup/ndscan

commit b2a292f1eb39a50cab064ad06002e524059d81f2

plots: Immediately check whether model already has schema/data
The previous design was an attempt to streamline the implementation by
having the models guarantee to emit a changed/… signal later (once
control was returned to the event loop, i.e. plots had a chance to
connect their slots), even if the model was already complete at
construction time.

This turned out to add more complexity than it removed, and did not work
properly for subscans (causing the user to have to switch points to
actually have the plot load).

After this, the plots themselves defer the initialisation until later,
as the calling code does not expect ready() to be immediately signalled.
This could be similarly simplified in the future.

ndscan/plots/model/hdf5.py
@@ -3,7 +3,6 @@ import logging
 from typing import Any
 import h5py
 from . import (Context, FixedDataSource, Model, Root, ScanModel, SinglePointModel)
-from .utils import call_later, emit_later
 from ...utils import SCHEMA_REVISION_KEY
 
 logger = logging.getLogger(__name__)
@@ -28,10 +27,6 @@ class HDF5Root(Root):
         axes = json.loads(datasets[prefix + "axes"][()])
         dim = len(axes)
 
-        # KLUDGE: Queue up this change event before constructing models, so that signals
-        # are emitted in order.
-        call_later(lambda: self.model_changed.emit(self._model))
-
         if dim == 0:
             self._model = HDF5SingleShotModel(datasets, prefix, schema_revision,
                                               context)
@@ -49,12 +44,10 @@ class HDF5SingleShotModel(SinglePointModel):
         super().__init__(schema_revision, context)
 
         self._channel_schemata = json.loads(datasets[prefix + "channels"][()])
-        emit_later(self.channel_schemata_changed, self._channel_schemata)
 
         self._point = {}
         for key in self._channel_schemata:
             self._point[key] = datasets[prefix + "point." + key][()]
-        emit_later(self.point_changed, self._point)
 
     def get_channel_schemata(self) -> dict[str, Any]:
         return self._channel_schemata
@@ -69,12 +62,9 @@ class HDF5ScanModel(ScanModel):
         super().__init__(axes, schema_revision, context)
 
         self._channel_schemata = json.loads(datasets[prefix + "channels"][()])
-        emit_later(self.channel_schemata_changed, self._channel_schemata)
 
-        call_later(lambda: self._set_online_analyses(
-            json.loads(datasets[prefix + "online_analyses"][()])))
-        call_later(lambda: self._set_annotation_schemata(
-            json.loads(datasets[prefix + "annotations"][()])))
+        self._set_online_analyses(json.loads(datasets[prefix + "online_analyses"][()]))
+        self._set_annotation_schemata(json.loads(datasets[prefix + "annotations"][()]))
 
         self._analysis_result_sources = {}
         ark = prefix + "analysis_results"
@@ -91,7 +81,6 @@ class HDF5ScanModel(ScanModel):
         for name in ([f"axis_{i}" for i in range(len(self.axes))] +
                      ["channel_" + c for c in self._channel_schemata.keys()]):
             self._point_data[name] = datasets[prefix + "points." + name][:]
-        emit_later(self.points_appended, self._point_data)
 
     def get_channel_schemata(self) -> dict[str, Any]:
         return self._channel_schemata

ndscan/plots/container_widgets.py
@@ -111,7 +111,9 @@ class RootWidget(QtWidgets.QWidget):
             # Ensure that the C++ object is deleted, which will also disconnect any of
             # the signals the plot used to track the model. Without this, we would
             # accumulate e.g. ever more invisible background updates when switching
-            # between subplot points.
+            # between subplot points. (For subplots, this is slightly suboptimal still,
+            # as the old model will still catch the rewrite; could optimise by
+            # explicitly disconnecting all the signals here and now.)
             self.plot_widget.deleteLater()
 
             self.plot_widget = None

ndscan/plots/image_2d.py
@@ -10,7 +10,7 @@ from . import colormaps
 from .cursor import CrosshairAxisLabel, CrosshairLabel, LabeledCrosshairCursor
 from .model import ScanModel
 from .plot_widgets import ContextMenuPanesWidget, add_source_id_label
-from .utils import (extract_linked_datasets, extract_scalar_channels,
+from .utils import (call_later, extract_linked_datasets, extract_scalar_channels,
                     format_param_identity, get_axis_scaling_info, setup_axis_item,
                     enum_to_numeric)
 
@@ -283,9 +283,15 @@ class Image2DPlotWidget(ContextMenuPanesWidget):
         self.found_duplicate_coords = False
         self.unique_coords = set[tuple[float, float]]()
 
+        if (channels := self.model.get_channel_schemata()) is not None:
+            call_later(lambda: self._initialise_series(channels))
+            if (points := self.model.get_point_data()) is not None:
+                call_later(lambda: self._update_points(points, False))
+
     def _initialise_series(self, channels):
         if self.plot is not None:
             self.plot_item.removeItem(self.plot.image_item)
+            self.plot.image_item.destroyLater()
             self.plot = None
 
         try:

ndscan/plots/model/subscan.py
@@ -3,7 +3,6 @@ import logging
 from typing import Any
 from ...utils import strip_suffix
 from . import (FixedDataSource, Model, Root, ScanModel, SinglePointModel)
-from .utils import call_later, emit_later
 
 logger = logging.getLogger(__name__)
 
@@ -73,10 +72,9 @@ class SubscanModel(ScanModel):
                     self._analysis_results[result_name] = source
                     self._analysis_result_mappings.append((result_name, channel_name))
 
-        emit_later(self.channel_schemata_changed, self._channel_schemata)
-        call_later(lambda: self._set_online_analyses(schema.get("online_analyses", {})))
-        call_later(lambda: self._set_annotation_schemata(schema.get("annotations", [])))
-        call_later(lambda: self._update(parent.get_point()))
+        self._set_online_analyses(schema.get("online_analyses", {}))
+        self._set_annotation_schemata(schema.get("annotations", []))
+        self._update(parent.get_point())
 
     def quit(self) -> None:
         self._parent.point_changed.disconnect(self._update)

ndscan/plots/model/subscriber.py
@@ -81,9 +81,7 @@ class SubscriberSinglePointModel(SinglePointModel):
         self._current_point = None
         self._next_point = {}
 
-    def get_channel_schemata(self) -> dict[str, Any]:
-        if self._channel_schemata is None:
-            raise ValueError("No complete point yet")
+    def get_channel_schemata(self) -> dict[str, Any] | None:
         return self._channel_schemata
 
     def get_point(self) -> dict[str, Any] | None:
@@ -213,7 +211,7 @@ class SubscriberScanModel(ScanModel):
     def get_annotations(self) -> list[Annotation]:
         return self._annotations
 
-    def get_channel_schemata(self) -> dict[str, Any]:
+    def get_channel_schemata(self) -> dict[str, Any] | None:
         return self._channel_schemata
 
     def get_point_data(self) -> dict[str, Any]:

ndscan/plots/model/utils.py
@@ -1,9 +1,0 @@
-from ..._qt import QtCore
-
-
-def call_later(func):
-    QtCore.QTimer.singleShot(0, func)
-
-
-def emit_later(signal, *args):
-    call_later(lambda: signal.emit(*args))

ndscan/plots/rolling_1d.py
@@ -8,7 +8,7 @@ from .model import SinglePointModel
 from .model.subscan import create_subscan_roots
 from .plot_widgets import (SubplotMenuPanesWidget, build_channel_selection_context_menu,
                            add_source_id_label)
-from .utils import (extract_scalar_channels, get_default_hidden_channels,
+from .utils import (call_later, extract_scalar_channels, get_default_hidden_channels,
                     group_channels_into_axes, group_axes_into_panes,
                     hide_series_from_groups, setup_axis_item, SERIES_COLORS)
 
@@ -89,6 +89,9 @@ class Rolling1DPlotWidget(SubplotMenuPanesWidget):
         self.data_names = None
         # Set of channel names that are currently hidden.
         self.hidden_channels = None
+
+        if self.model.get_channel_schemata() is not None:
+            call_later(self._initialise_series)
 
     def _initialise_series(self):
         self.clear_panes()

ndscan/plots/utils.py
@@ -2,6 +2,7 @@ import html
 import logging
 from typing import Any
 from ..utils import eval_param_default
+from .._qt import QtCore
 
 logger = logging.getLogger(__name__)
 
@@ -396,3 +397,7 @@ def enum_to_numeric(categories, values: list[Any]):
         return [to_idx[x] for x in values]
     except KeyError:
         raise KeyError("Unexpected enum value found.")
+
+
+def call_later(func):
+    QtCore.QTimer.singleShot(0, func)

commit 3a9a45d8d854f0c078cf9594c4b67a3c2ee40f7f

plots: Fix HDF5 loading error when default annotations present
This was a regression introduced in
b2a292f1eb39a50cab064ad06002e524059d81f2 which removed swathes of
call_later() – without it, the annotations were processed before the
points were set.

ndscan/plots/model/hdf5.py
@@ -63,9 +63,6 @@ class HDF5ScanModel(ScanModel):
 
         self._channel_schemata = json.loads(datasets[prefix + "channels"][()])
 
-        self._set_online_analyses(json.loads(datasets[prefix + "online_analyses"][()]))
-        self._set_annotation_schemata(json.loads(datasets[prefix + "annotations"][()]))
-
         self._analysis_result_sources = {}
         ark = prefix + "analysis_results"
         if ark in datasets:
@@ -81,6 +78,9 @@ class HDF5ScanModel(ScanModel):
         for name in ([f"axis_{i}" for i in range(len(self.axes))] +
                      ["channel_" + c for c in self._channel_schemata.keys()]):
             self._point_data[name] = datasets[prefix + "points." + name][:]
+
+        self._set_online_analyses(json.loads(datasets[prefix + "online_analyses"][()]))
+        self._set_annotation_schemata(json.loads(datasets[prefix + "annotations"][()]))
 
     def get_channel_schemata(self) -> dict[str, Any]:
         return self._channel_schemata